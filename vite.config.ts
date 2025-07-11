23ad387939c9b29717735ab85182c39680e94381
Hyun Heechan <hyeonhc328@gmail.com>
2025-07-11 17:14:27 +0900

    chore: initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,45 @@
 /// <reference types="vitest/config" />
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import svgr from 'vite-plugin-svgr'
 import * as path from 'node:path'
-import { fileURLToPath } from 'node:url'
 
 import AutoImport from 'unplugin-auto-import/vite'
 import Icons from 'unplugin-icons/vite'
 import IconsResolver from 'unplugin-icons/resolver'
-import Components from 'unplugin-react-components/vite'
-import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
+
+import {fileURLToPath} from 'node:url'
+import {storybookTest} from '@storybook/addon-vitest/vitest-plugin'
 
 const dirname =
   typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url))
 
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [
-    react(),
-    svgr(),
-
+  plugins: [react(), svgr(),
     AutoImport({
-      imports: [
-        'react',
+      imports: ['react', // preset: useState, useEffect 등
         {
           'react-router-dom': ['useNavigate', 'useLocation'],
-        },
-      ],
-      dts: './src/types/auto-imports.d.ts',
+          dayjs: [['default', 'dayjs']],
+        },],
+      dts: 'src/auto-imports.d.ts',
       eslintrc: {
         enabled: true,
         filepath: './.eslintrc-auto-import.json',
         globalsPropValue: true,
       },
-      resolvers: [IconsResolver({ prefix: 'Icon' })],
-    }),
-
-    Components({
-      dts: './src/types/components.d.ts',
       resolvers: [
-        IconsResolver({
-          prefix: 'Icon',
-          extension: 'jsx',
-          enabledCollections: ['mdi'], // @iconify-json/mdi 설치 필요
-        }),
+        IconsResolver({prefix: 'Icon'}),
       ],
     }),
-
     Icons({
+      autoInstall: true,
       compiler: 'jsx',
-      jsx: 'react',
     }),
+
   ],
   resolve: {
     alias: {
@@ -65,17 +53,25 @@ export default defineConfig({
       {
         extends: true,
         plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
           storybookTest({
             configDir: path.join(dirname, '.storybook'),
           }),
         ],
+
+
         test: {
           name: 'storybook',
           browser: {
             enabled: true,
             headless: true,
             provider: 'playwright',
-            instances: [{ browser: 'chromium' }],
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
           },
           setupFiles: ['.storybook/vitest.setup.ts'],
         },
