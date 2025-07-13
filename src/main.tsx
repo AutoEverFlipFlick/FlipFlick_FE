3ea894dda057266c5d668adc52e36134dac02c44
Choo ChangWoo <cnckddn0146@gmail.com>
2025-07-13 21:55:54 +0900

    feat(Project): :lipstick: 전역 스타일 지정
    normalize.css를  적용하여 브라우저간 통일 스타일을 작성

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+// nomalize.css 설명
+// 브라우저의 내장 스타일을 최대한 건들지 않는 선에서
+// 브라우저간 상이한 스타일 통일
+import '@/styles/normalize.css'
+
+// 프로젝트 전역 스타일 부분
+// 폰트, 컬러팔레트, 간격 등
+import '@/styles/base.css'
+
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-    <App />
-)
+createRoot(document.getElementById('root')!).render(<App />)
