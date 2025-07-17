631867a99a9903f2a2fc740c941f90777c0b08c4
Choo ChangWoo <cnckddn0146@gmail.com>
2025-07-17 22:29:56 +0900

    feat(Header): :sparkles: 헤더 구현

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -43,7 +43,7 @@
 
 // export default AppRoutes
 
-import { Route, Routes,Navigate } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import ExamplePage from '@/pages/example/ExamplePage'
 import PlaylistPage from '@/pages/playlist/playlist'
 import CreatePlaylist from '@/pages/playlist/CreatePlaylist'
@@ -59,14 +59,21 @@ import Login from '@/pages/member/Login'
 import SignUp from '@/pages/member/SignUp'
 import EmailLogin from '@/pages/member/EmailLogin'
 import KakaoRedirectHandler from '@/pages/member/KakaoRedirectHandler'
-// import Layout from '@/components/common/CommonLayout' // 공통 레이아웃 컴포넌트가 필요함
+import Layout from '@/components/common/layout/BasePageLayout' // 공통 레이아웃 컴포넌트가 필요함
 
 const AppRoutes = () => {
   return (
     <AuthProvider>
       <Routes>
         {/* Public Routes */}
-        <Route path="/" element={<ExamplePage />} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <ExamplePage />
+            </Layout>
+          }
+        />
         <Route path="/playlist" element={<PlaylistPage />} />
         <Route
           path="/playlist/:id"
@@ -80,26 +87,50 @@ const AppRoutes = () => {
         <Route path="/totalsearch" element={<TotalSearch />} />
 
         {/* Public Routes (로그인된 사용자는 접근 불가) */}
-        <Route path="/login" element={
-          <PublicRoute><Login /></PublicRoute>
-          
-        } />
-        <Route path="/signup" element={
-          <PublicRoute><SignUp /></PublicRoute>
-        } />
-        <Route path="/emaillogin" element={
-          <PublicRoute><EmailLogin /></PublicRoute>
-        } />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <SignUp />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/emaillogin"
+          element={
+            <PublicRoute>
+              <EmailLogin />
+            </PublicRoute>
+          }
+        />
 
         {/* Protected Routes (로그인 필요) */}
-        <Route path="/createplaylist" element={
-          <ProtectedRoute><CreatePlaylist /></ProtectedRoute>
-        } />
-        <Route path="/playlist/:id/edit" element={
-          <ProtectedRoute><EditPlaylist /></ProtectedRoute>
-        } />
+        <Route
+          path="/createplaylist"
+          element={
+            <ProtectedRoute>
+              <CreatePlaylist />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/playlist/:id/edit"
+          element={
+            <ProtectedRoute>
+              <EditPlaylist />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
-        
+
         <Route path="/oauth/kakao/callback" element={<KakaoRedirectHandler />} />
       </Routes>
     </AuthProvider>
